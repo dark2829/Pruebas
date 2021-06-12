2a27e87606944ff31620c8c69d2724b785eded32
dark2829 <tozcuento.2829@gmail.com>
2021-06-12 16:45:01 -0500

    Operaciones basicas con radio button (Suma, resta).

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,40 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText fis, mat, qui, esp, pro;
-    private TextView sal;
-
+    private EditText itxt_uno, itxt_dos;
+    private TextView res;
+    private RadioButton rb_suma, rb_resta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Actividad creada", Toast.LENGTH_SHORT);//Actividad creada.
 
-        this.fis = (EditText)findViewById(R.id.itxt_fis);
-        this.mat = (EditText)findViewById(R.id.itxt_mat);
-        this.qui = (EditText)findViewById(R.id.itxt_qui);
-        this.esp = (EditText)findViewById(R.id.itxt_esp);
-        this.pro = (EditText)findViewById(R.id.itxt_pro);
-        this.sal = (TextView)findViewById(R.id.otxt_titulo);
+        this.itxt_uno = (EditText)findViewById(R.id.itxt_numu);
+        this.itxt_dos = (EditText)findViewById(R.id.itxt_numd);
+        this.res = (TextView)findViewById(R.id.otxt_res);
+        this.rb_suma = (RadioButton)findViewById(R.id.rb_su);
+        this.rb_resta = (RadioButton)findViewById(R.id.rb_re);
     }
 
-    public void estatus(View view) {
-        float mat_flo = Integer.parseInt(mat.getText().toString());
-        float qui_flo = Integer.parseInt(qui.getText().toString());
-        float esp_flo = Integer.parseInt(esp.getText().toString());
-        float pro_flo = Integer.parseInt(pro.getText().toString());
-        float fis_flo = Integer.parseInt(fis.getText().toString());
-
-        float sal_flo = (mat_flo + qui_flo + esp_flo + pro_flo + fis_flo) / 5;
-
-        if (sal_flo >= 6){
-            this.sal.setText("Estatus aprovado con "+sal_flo);
+    //Metodo para el boton caclular
+    public void operaciones(View view){
+        float itxt_uno_float = Float.parseFloat(itxt_uno.getText().toString());
+        float itxt_dos_float = Float.parseFloat(itxt_dos.getText().toString());
+        float operacion = 0;
+        String resultado = "";
+        if (rb_resta.isChecked() == true){
+            operacion = itxt_uno_float - itxt_dos_float;
         }else{
-            this.sal.setText("Estatus reprobado con "+sal_flo);
+            operacion = itxt_uno_float + itxt_dos_float;
         }
+        resultado = String.valueOf(operacion);
+        this.res.setText(resultado);
     }
 }
