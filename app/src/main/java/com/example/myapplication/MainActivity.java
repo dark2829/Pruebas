4e341aab1e65346a286612fafa71f3c2c5d0a127
dark2829 <tozcuento.2829@gmail.com>
2021-06-12 23:07:40 -0500

    Calculadora con spinner.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText itxt_uno, itxt_dos;
     private TextView res;
-    private CheckBox cb_suma, cb_resta, cb_multi, cb_divi;
+    private Spinner spi_1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,38 +26,37 @@ public class MainActivity extends AppCompatActivity {
         this.itxt_uno = (EditText)findViewById(R.id.itxt_numu);
         this.itxt_dos = (EditText)findViewById(R.id.itxt_numd);
         this.res = (TextView)findViewById(R.id.otxt_res);
-        this.cb_suma = (CheckBox)findViewById(R.id.cb_su);
-        this.cb_resta = (CheckBox)findViewById(R.id.cb_re);
-        this.cb_multi = (CheckBox)findViewById(R.id.cb_mu);
-        this.cb_divi = (CheckBox)findViewById(R.id.cb_di);
+        spi_1 = (Spinner)findViewById(R.id.sp_op);
+
+        String [] operaciones = {"Sumar", "Restar", "Multiplicar", "Dividir"};
+        //crear un objeto y agregarle parametros de dondese quiere el spinner.
+        //donde, tipo, nombre de vector
+        ArrayAdapter <String> arr_ope = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, operaciones);
+        spi_1.setAdapter((arr_ope));//mostrar el spinner
     }
 
     //Metodo para el boton caclular
-    public void operaciones(View view){
-        float itxt_uno_float = Float.parseFloat(itxt_uno.getText().toString());
-        float itxt_dos_float = Float.parseFloat(itxt_dos.getText().toString());
-        float operacion = 0;
-        String resultado = "";
-        if (cb_resta.isChecked() == true) {
-            operacion = itxt_uno_float - itxt_dos_float;
-            resultado = "La resta es:"+operacion+" / ";
-        }
-        if (cb_suma.isChecked() == true) {
-            operacion = itxt_uno_float + itxt_dos_float;
-            resultado += "La suma es:"+operacion+" /";
-        }
-        if (cb_multi.isChecked() == true) {
-            operacion = itxt_uno_float * itxt_dos_float;
-            resultado += "La multiplicacion es:"+operacion+" / ";
-        }
-        if (cb_divi.isChecked() == true) {
-            if (itxt_dos_float != 0) {
-                operacion = itxt_uno_float / itxt_dos_float;
-                resultado += "La division es:"+operacion+" / ";
-            } else {
-                Toast.makeText(this, "Ingresar un numero diferente de 0 en el segundo numero", Toast.LENGTH_LONG).show();
+    public void calcular(View view){
+        float itxt_float_uno = Float.parseFloat(this.itxt_uno.getText().toString());
+        float itxt_float_dos = Float.parseFloat(this.itxt_dos.getText().toString());
+        String selection = spi_1.getSelectedItem().toString();//castear la operacion seleccionada
+        float result = 0;
+        String res_tex = "";
+        if(selection.equals("Sumar")){
+            result = itxt_float_uno + itxt_float_dos;
+        }else if(selection.equals("Restar")){
+            result = itxt_float_uno - itxt_float_dos;
+        }else if(selection.equals("Multiplicar")){
+            result = itxt_float_uno * itxt_float_dos;
+        }else if(selection.equals("Dividir")){
+            if(itxt_float_dos != 0){
+                result = itxt_float_uno / itxt_float_dos;
+                Toast.makeText(this,"Introduce un numero difernete de 0", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this,"Introduce un numero difernete de 0", Toast.LENGTH_LONG).show();
             }
         }
-        this.res.setText(resultado);
+        res_tex = String.valueOf(result);
+        this.res.setText(res_tex);
     }
 }
